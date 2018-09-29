8df3329caabe873904eb82290ea51611dcf13738
tianqisk8 <tianqi19955@126.com>
2018-09-29 17:38:50 +0800

    update Profile.go

--- model/profile.go
+++ model/profile.go
@@ -1,6 +1,17 @@
 package model
 
 type Profile struct {
-	name string
-	
+	name 			string
+	Gender 			string
+	Age 			int
+	Heigth			int
+	Weigth			int
+	Income 			string
+	Marriage    	string
+	Education   	string
+	Occupation  	string
+	Hokou 			string
+	Xinzuo 			string
+	House 			string
+	Car 			string
 }
