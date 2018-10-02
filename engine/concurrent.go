363471824c6f8e86d798b7c0cdbcea97d9cf3167
tianqisk8 <tianqi19955@126.com>
2018-10-02 17:50:37 +0800

    Queued

--- engine/concurrent.go
+++ engine/concurrent.go
@@ -1,8 +1,6 @@
 package engine
 
-import (
-	"log"
-)
+import "log"
 
 type ConCurrentEngine struct {
 	Scheduler Scheduler
@@ -12,15 +10,42 @@ type ConCurrentEngine struct {
 type Scheduler interface {
 	Submit(request Request)
 	ConfigureMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
 }
 
 func (e *ConCurrentEngine) Run(seeds ...Request)  {
-	in := make(chan Request)
+	//in := make(chan Request)
+	//out := make(chan ParseResult)
+	//e.Scheduler.ConfigureMasterWorkerChan(in)
+	//
+	//for i := 0; i < e.WorkerCount; i++ {
+	//	createWorker(in, out)
+	//}
+	//
+	//for _, r := range seeds {
+	//	e.Scheduler.Submit(r)
+	//}
+	//
+	//itemCount := 0
+	//for {
+	//	result := <-out
+	//	for _, item := range result.Items {
+	//		log.Printf("Got item #%d: %v", itemCount, item)
+	//		itemCount++
+	//	}
+	//
+	//	for _, request := range result.Requests {
+	//		e.Scheduler.Submit(request)
+	//	}
+	//}
+
 	out := make(chan ParseResult)
-	e.Scheduler.ConfigureMasterWorkerChan(in)
+	e.Scheduler.Run()
 
 	for i := 0; i < e.WorkerCount; i++ {
-		createWorker(in, out)
+		//createWorker(in, out)
+		createWorker(out, e.Scheduler)
 	}
 
 	for _, r := range seeds {
@@ -41,9 +66,27 @@ func (e *ConCurrentEngine) Run(seeds ...Request)  {
 	}
 }
 
-func createWorker(in chan Request, out chan ParseResult) {
+//func createWorker(in chan Request, out chan ParseResult) {
+//	go func() {
+//		for {
+//			//tell scheduler i'm ready
+//			request := <-in
+//			result, err := worker(request)
+//			if err != nil {
+//				continue
+//			}
+//
+//			out <- result
+//		}
+//	}()
+//}
+
+func createWorker(out chan ParseResult, s Scheduler) {
+	in := make(chan Request)
 	go func() {
 		for {
+			//tell scheduler i'm ready
+			s.WorkerReady(in)
 			request := <-in
 			result, err := worker(request)
 			if err != nil {
---
+++ scheduler/queued.go
@@ -0,0 +1,47 @@
+package scheduler
+
+import "crawler/engine"
+
+type QueuedScheduler struct {
+	requestChan chan engine.Request
+	workerChan chan chan engine.Request
+}
+
+func (qs *QueuedScheduler) Submit(request engine.Request) {
+	qs.requestChan <- request
+}
+
+func (qs *QueuedScheduler) WorkerReady(w chan engine.Request) {
+	qs.workerChan <- w
+}
+
+func (qs *QueuedScheduler) ConfigureMasterWorkerChan(chan engine.Request) {
+
+}
+
+func (qs *QueuedScheduler) Run() {
+	qs.workerChan  = make(chan chan engine.Request)
+	qs.requestChan = make(chan engine.Request)
+	go func() {
+		var requestQ []engine.Request
+		var workerQ  []chan engine.Request
+		for {
+			var activeRequest engine.Request
+			var activeWorker chan engine.Request
+			if len(requestQ) > 0 && len(workerQ) > 0 {
+				activeRequest = requestQ[0]
+				activeWorker  = workerQ[0]
+			}
+			select {
+			case r := <-qs.requestChan:
+				requestQ = append(requestQ, r)
+			case w := <-qs.workerChan:
+				workerQ = append(workerQ, w)
+			case activeWorker <- activeRequest:
+				requestQ = requestQ[1:]
+				workerQ  = workerQ[1:]
+			}
+		}
+	}()
+}
+
